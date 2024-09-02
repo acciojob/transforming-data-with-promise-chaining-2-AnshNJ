7d6a362724137aa1f12a0ebe09b7cb4d37d37513
Ansh Jain <anshjain866@gmail.com>
2024-09-02 12:15:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-//your JS code here. If required.
+const inputField = document.getElementById("ip");
+const button = document.getElementById("btn");
+const result = document.getElementById("output");
+
+button.addEventListener("click", promiseHandler);
+
+function promiseHandler() {
+	const myPromise = new Promise((resolve, reject) => {
+		const num = parseInt(inputField.value);
+		setTimeout(() => {
+			result.innerHTML = `Result: ${num}`;
+			resolve(num);
+		}, 2000);
+	});
+
+	myPromise
+		.then((num) => {
+			num *= 2;
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					result.innerHTML = `Result: ${num}`;
+					resolve(num);
+				}, 1000);
+			});
+		})
+		.then((num) => {
+			num -= 3;
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					result.innerHTML = `Result: ${num}`;
+					resolve(num);
+				}, 1000);
+			});
+		})
+		.then((num) => {
+			num /= 2;
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					result.innerHTML = `Result: ${num}`;
+					resolve(num);
+				}, 1000);
+			});
+		})
+		.then((num) => {
+			num += 10;
+			return new Promise((resolve) => {
+				setTimeout(() => {
+					result.innerHTML = `Result: ${num}`;
+					resolve(num);
+				}, 1000);
+			});
+		})
+		.then((num) => {
+			setTimeout(() => {
+				result.innerHTML = `Final Result: ${num}`;
+			}, 1000);
+		})
+		.catch((error) => alert(error));
+}
